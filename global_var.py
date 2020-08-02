8b4e721857da6e6dedb84c0cc69cfb0de6a1e562
Prerna Gupta <47175800+prrsangupta123@users.noreply.github.com>
2020-08-03 01:24:06 +0530

    Add files via upload

---
+++ global_var.py
@@ -0,0 +1,23 @@
+import background
+
+# Create scene
+scenery = background.Scene()
+# Name of person playing the game
+username = "Player"
+quit_flag = 0
+bullet_present = False
+bullet_dr = False
+score = 0
+lives = 10
+dragon_lives = 10
+shield_time = 0
+boost = 0
+shield=0
+boost1=0
+shield1=0
+magnet_pos=0
+lives1=0
+bullets1_present = False
+bullets_present = False
+is_dragon=0
+shield_cooldown=0
